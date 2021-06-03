1435bb415ad05a87fa5af14f6c86bf7308741b14
lebedchenkov <81518728+lebedchenkov@users.noreply.github.com>
2021-06-03 13:52:03 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/Page1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            Entry Entry1 = new Entry()
+            {
+                Placeholder = "Write",
+                TextColor = Color.Blue
+            };
+            Button Button1 = new Button()
+            {
+                Text = "Tap"
+            };
+
+            Content = new StackLayout
+            {
+                Children = {
+                    Entry1,
+                    Button1
+                }
+            };
+        }
+    }
+}         
